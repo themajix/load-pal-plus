ec849b4478c891345df1f3d64683716b03c9446a
Majid Shams <majid.shams@gmail.com>
2019-04-24 18:03:11 -0400

    part-2 from the web

--- app.js
+++ app.js
@@ -50,6 +50,8 @@
 // }
 
 var constraints = { video: { facingMode: "user" }, audio: false };
+var track = null;
+
 const cameraView = document.querySelector("#camera--view"),
       cameraOutput = document.querySelector("#camera--output"),
       cameraSensor = document.querySelector("#camera--sensor"),
@@ -77,3 +79,15 @@ cameraTrigger.onclick = function() {
 
 window.addEventListener("load", cameraStart, false);
 
+// Install ServiceWorker
+if ('serviceWorker' in navigator) {
+    console.log('CLIENT: service worker registration in progress.');
+    navigator.serviceWorker.register( '/camera-app/part-2/sw.js' , { scope : ' ' } ).then(function() {
+      console.log('CLIENT: service worker registration complete.');
+    }, function() {
+      console.log('CLIENT: service worker registration failure.');
+    });
+  } else {
+    console.log('CLIENT: service worker is not supported.');
+  }
+  

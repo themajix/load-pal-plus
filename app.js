4fcf544d773383283bea266307b4ae9166bbc116
Majid Shams <majid.shams@gmail.com>
2019-04-25 09:22:19 -0400

    updated files

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var track = null;
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger");
+    cameraTrigger = document.querySelector("#camera--trigger"),
+    cameraSave = document.querySelector("#camera--save"),
+    cameraOpen = document.querySelector("#camera--open");
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -29,6 +31,27 @@ cameraTrigger.onclick = function() {
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
     // track.stop();
+};
+
+// Save a picture when cameraSave is tapped
+DefaultEndpointsProtocol=https;AccountName=storagelpp;AccountKey=PVyBSdVxj955/H7bVfAT26jSAtDHwsKrJC+eyte/C7R/+FEoiEmkD5Ueo9OBaRAxTMWpkan0GIPOmnOyP0fyXg==;EndpointSuffix=core.windows.net
+
+cameraSave.onclick = function() {
+  var xmlHttp = getNewHTTPObject();
+  chargeURLPut('https://storagelpp.blob.core.windows.net/imageslpp/myblob');
+
+  function chargeURLPut(url) { 
+      var savedImage = cameraSensor.getContext("2d").getImageData(0, 0, cameraSensor.width, cameraSensor.height);
+      var mimeType = "image/webp";  
+      xmlHttp.open('PUT', url, true);  // true : asynchrone false: synchrone
+      xmlHttp.setRequestHeader('Content-Type', mimeType);  
+      xmlHttp.setRequestHeader('x-ms-blob-content-disposition', 'attachment; filename="fname.ext"');  
+      xmlHttp.setRequestHeader('x-ms-blob-type', 'BlockBlob' );  
+      xmlHttp.setRequestHeader('x-ms-meta-m1', 'v1' );  
+      xmlHttp.setRequestHeader('x-ms-meta-m2', 'v2' );  
+      xmlHttp.setRequestHeader('Authorization', 'SharedKey storagelpp:PVyBSdVxj955/H7bVfAT26jSAtDHwsKrJC+eyte/C7R/+FEoiEmkD5Ueo9OBaRAxTMWpkan0GIPOmnOyP0fyXg==');  
+      xmlHttp.send(savedImage); 
+  }
 };
 
 // Start the video stream when the window loads

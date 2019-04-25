a6f79d2490214b95ad9fcc400d00c427f88ab46a
Majid Shams <majid.shams@gmail.com>
2019-04-25 08:00:27 -0400

    got files from part 2

--- app.js
+++ app.js
@@ -1,93 +1,49 @@
-// const apiKey = 'd473fbb2fe9547a19b3d51e31d246441';
-// const defaultSource = 'the-washington-post';
-// const sourceSelector = document.querySelector('#sources');
-// const newsArticles = document.querySelector('main');
-
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () =>
-//     navigator.serviceWorker.register('sw.js')
-//       .then(registration => console.log('Service Worker registered'))
-//       .catch(err => 'SW registration failed'));
-// }
-
-// window.addEventListener('load', e => {
-//   sourceSelector.addEventListener('change', evt => updateNews(evt.target.value));
-//   updateNewsSources().then(() => {
-//     sourceSelector.value = defaultSource;
-//     updateNews();
-//   });
-// });
-
-// window.addEventListener('online', () => updateNews(sourceSelector.value));
-
-// async function updateNewsSources() {
-//   const response = await fetch(`https://newsapi.org/v2/sources?apiKey=${apiKey}`);
-//   const json = await response.json();
-//   sourceSelector.innerHTML =
-//     json.sources
-//       .map(source => `<option value="${source.id}">${source.name}</option>`)
-//       .join('\n');
-// }
-
-// async function updateNews(source = defaultSource) {
-//   newsArticles.innerHTML = '';
-//   const response = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&sortBy=top&apiKey=${apiKey}`);
-//   const json = await response.json();
-//   newsArticles.innerHTML =
-//     json.articles.map(createArticle).join('\n');
-// }
-
-// function createArticle(article) {
-//   return `
-//     <div class="article">
-//       <a href="${article.url}">
-//         <h2>${article.title}</h2>
-//         <img src="${article.urlToImage}" alt="${article.title}">
-//         <p>${article.description}</p>
-//       </a>
-//     </div>
-//   `;
-// }
-
+// Set constraints for the video stream
 var constraints = { video: { facingMode: "user" }, audio: false };
 var track = null;
 
+// Define constants
 const cameraView = document.querySelector("#camera--view"),
-      cameraOutput = document.querySelector("#camera--output"),
-      cameraSensor = document.querySelector("#camera--sensor"),
-      cameraTrigger = document.querySelector("#camera--trigger")
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
 
+// Access the device camera and stream to cameraView
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
         .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
 }
 
+// Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
+    // track.stop();
 };
 
+// Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
 
 // Install ServiceWorker
 if ('serviceWorker' in navigator) {
-    console.log('CLIENT: service worker registration in progress.');
-    navigator.serviceWorker.register( 'sw.js' , { scope : ' ' } ).then(function() {
-      console.log('CLIENT: service worker registration complete.');
-    }, function() {
-      console.log('CLIENT: service worker registration failure.');
-    });
-  } else {
-    console.log('CLIENT: service worker is not supported.');
-  }
-  
+  console.log('CLIENT: service worker registration in progress.');
+  navigator.serviceWorker.register( 'sw.js' , { scope : ' ' } ).then(function() {
+    console.log('CLIENT: service worker registration complete.');
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+} else {
+  console.log('CLIENT: service worker is not supported.');
+}
+
--- sw.js
+++ sw.js
@@ -1,50 +1,3 @@
-// const cacheName = 'news-v1';
-
-// const staticAssets = [
-//   './',
-//   './app.js',
-//   './styles.css',
-//   './fallback.json',
-//   './images/fetch-dog.jpg'
-// ];
-
-// self.addEventListener('install', async function () {
-//   const cache = await caches.open(cacheName);
-//   cache.addAll(staticAssets);
-// });
-
-// self.addEventListener('activate', event => {
-//   event.waitUntil(self.clients.claim());
-// });
-
-// self.addEventListener('fetch', event => {
-//   const request = event.request;
-//   const url = new URL(request.url);
-//   if (url.origin === location.origin) {
-//     event.respondWith(cacheFirst(request));
-//   } else {
-//     event.respondWith(networkFirst(request));
-//   }
-// });
-
-// async function cacheFirst(request) {
-//   const cachedResponse = await caches.match(request);
-//   return cachedResponse || fetch(request);
-// }
-
-// async function networkFirst(request) {
-//   const dynamicCache = await caches.open('news-dynamic');
-//   try {
-//     const networkResponse = await fetch(request);
-//     dynamicCache.put(request, networkResponse.clone());
-//     return networkResponse;
-//   } catch (err) {
-//     const cachedResponse = await dynamicCache.match(request);
-//     return cachedResponse || await caches.match('./fallback.json');
-//   }
-// }
-
-
 "use strict";
 
 console.log('WORKER: executing.');
@@ -59,10 +12,10 @@ var version = 'v1::';
    then the service worker won't be installed either.
 */
 var offlineFundamentals = [
-  './load-pal-plus/',
-  './load-pal-plus/index.html',
-  './load-pal-plus/style.css',
-  './load-pal-plus/app.js'
+  '',
+  'index.html',
+  'style.css',
+  'app.js'
 ];
 
 /* The install event fires when the service worker is first installed.
@@ -94,9 +47,6 @@ self.addEventListener("install", function(event) {
       .then(function() {
         console.log('WORKER: install completed');
       })
-     .catch(function(err) {
-      console.error(err);
-     })
   );
 });
 

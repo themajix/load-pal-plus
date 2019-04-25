d2309b23e6a73092bbd44b71e3f663b25ca21c44
Majid Shams <majid.shams@gmail.com>
2019-04-25 09:37:20 -0400

    fix http client

--- app.js
+++ app.js
@@ -56,6 +56,47 @@ cameraSave.onclick = function() {
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
 
+function getNewHTTPObject()
+{
+        var xmlhttp;
+
+        /** Special IE only code ... */
+        /*@cc_on
+          @if (@_jscript_version >= 5)
+              try
+              {
+                  xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+              }
+              catch (e)
+              {
+                  try
+                  {
+                      xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                  }
+                  catch (E)
+                  {
+                      xmlhttp = false;
+                  }
+             }
+          @else
+             xmlhttp = false;
+        @end @*/
+
+        /** Every other browser on the planet */
+        if (!xmlhttp && typeof XMLHttpRequest != 'undefined')
+        {
+            try
+            {
+                xmlhttp = new XMLHttpRequest();
+            }
+            catch (e)
+            {
+                xmlhttp = false;
+            }
+        }
+
+        return xmlhttp;
+}
 
 // Install ServiceWorker
 if ('serviceWorker' in navigator) {
